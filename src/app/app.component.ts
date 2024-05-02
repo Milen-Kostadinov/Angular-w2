e44a685dcfdea3899d44bc3ddb9d5c541bc2f815
User <stu2101261018@uni-plovdiv.bg>
2024-05-02 23:59:25 +0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.scss 	deleted:    src/app/app.component.spec.ts 	modified:   src/app/app.component.ts 	deleted:    src/app/app.config.server.ts 	deleted:    src/app/app.config.ts 	deleted:    src/app/app.routes.ts 	new file:   src/assets/images.jpg 	new file:   src/assets/images.png 	new file:   src/assets/star.png 	modified:   src/main.server.ts 	modified:   src/main.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,90 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular-w2';
+  reviewCompleted = false;
+  index = 0
+  index2 = 0
+  books = [
+    {
+      name: "Lord of the rings: The fellowshit of the ring",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      author: "J.R.R.Tolkein",
+      averageRating: 0,
+      allRatings: [2,3,4,5,6,2]
+    },
+    {
+      name: "Lord of the rings: The two towers",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      author: "J.R.R.Tolkein",
+      averageRating: 0,
+      allRatings: [2,2]
+    },
+    {
+      name: "Lord of the rings: The return of the king",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      author: "J.R.R.Tolkein",
+      averageRating: 0,
+      allRatings: [2,2]
+    },
+    {
+      name: "Harry potter and prisoner of askaban",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      author: "J.K.Rowling",
+      averageRating: 0,
+      allRatings: [2,2]
+    },
+    {
+      name: "Harry potter and the deadly hollows part 1",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      author: "J.K.Rowling",
+      averageRating: 0,
+      allRatings: [2,2]
+    },
+    {
+      name: "Harry potter and the deadly hollows part 2",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      author: "J.K.Rowling",
+      averageRating: 0,
+      allRatings: [2,2]
+    },
+    {
+      name: "Harry potter and the philosophers stone",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      author: "J.K.Rowling",
+      averageRating: 0,
+      allRatings: [2,2]
+    }
+  ]
+  public currentBook = this.books[this.index]
+  public addNewReview(rating: number){
+    this.books[this.index].allRatings[this.books[this.index].allRatings.length] = rating;
+    let sum = 0
+    for(let i = 0; i < this.books[this.index].allRatings.length; i++){
+      sum+= this.books[this.index].allRatings[i]
+    }
+    this.books[this.index].averageRating = sum / this.books[this.index].allRatings.length
+    console.log(this.books.length)
+    console.log(this.index)
+    if(this.index >= this.books.length - 1)
+    {
+      this.reviewCompleted = true
+      this.index = 0;
+    }
+    else{
+      this.index++
+    }
+  }
+  public review(){
+    this.reviewCompleted = false;
+  }
+  link = ""
+  public getVideo(){
+    this.link = "https://youtu.be/iQzqxXzeGRU?si=pWEiKL8kkpQEmn9n"
+  }
 }
